9f7df1fd4b61e70e75c65324955eb349d51744a9
Eduardo Derisso <dudu.derisso@gmail.com>
2024-12-02 01:49:17 -0300

    feat: added hooks to add and edit entities

--- src/hooks/addData/addData.js
+++ src/hooks/addData/addData.js
@@ -2,29 +2,29 @@ import { refreshTableContext } from "../../contexts/appContext";
 import { useContext } from "react";
 import useCrudOperations from "../useCrudOperations/useCrudOperations";
 
-export default function useDeleteData() {
+export default function useAddData() {
     const {refreshTable, setRefreshTable} = useContext(refreshTableContext)
     const { performCrudOperation, loading, error } = useCrudOperations();
 
-    async function deleteData( itemType, itemId) {
+    async function addData( itemType) {
 
-        const endpoint = `${itemType}/${itemId}`;
+        const endpoint = `${itemType}/`;
         
         console.log("endpoint: ", endpoint);
 
         try {
-            const response = await performCrudOperation(endpoint, "delete");
+            const response = await performCrudOperation(endpoint, "post");
             console.log(response);
             if (response && response.status === 200) setRefreshTable(!refreshTable);
             return response.data;
         } catch (error) {
-            console.log(`Erro ao delete ${itemType}:`, error);
+            console.log(`Erro ao add ${itemType}:`, error);
             if (error.response) {
                 // A resposta do servidor veio com um código de status de erro
-                console.log(`Erro ao deletar ${itemType}: `, error.response.status, error.response.data);
+                console.log(`Erro ao adicionar ${itemType}: `, error.response.status, error.response.data);
               } else if (error.request) {
                 // A requisição foi feita mas não houve resposta
-                console.log(`Erro ao deletar ${itemType}:  Sem resposta do servidor`);
+                console.log(`Erro ao adicionar ${itemType}:  Sem resposta do servidor`);
               } else {
                 // Outro tipo de erro ocorreu
                 console.log("Erro desconhecido: ", error.message);
@@ -33,6 +33,6 @@ export default function useDeleteData() {
               
     }
     
-    return deleteData;
+    return addData;
 
 }
--- src/hooks/saveData/saveData.js
+++ src/hooks/saveData/saveData.js
@@ -2,29 +2,29 @@ import { refreshTableContext } from "../../contexts/appContext";
 import { useContext } from "react";
 import useCrudOperations from "../useCrudOperations/useCrudOperations";
 
-export default function useDeleteData() {
+export default function useSaveData() {
     const {refreshTable, setRefreshTable} = useContext(refreshTableContext)
     const { performCrudOperation, loading, error } = useCrudOperations();
 
-    async function deleteData( itemType, itemId) {
+    async function saveData( itemType, itemId) {
 
         const endpoint = `${itemType}/${itemId}`;
         
         console.log("endpoint: ", endpoint);
 
         try {
-            const response = await performCrudOperation(endpoint, "delete");
+            const response = await performCrudOperation(endpoint, "put");
             console.log(response);
             if (response && response.status === 200) setRefreshTable(!refreshTable);
             return response.data;
         } catch (error) {
-            console.log(`Erro ao delete ${itemType}:`, error);
+            console.log(`Erro ao save ${itemType}:`, error);
             if (error.response) {
                 // A resposta do servidor veio com um código de status de erro
-                console.log(`Erro ao deletar ${itemType}: `, error.response.status, error.response.data);
+                console.log(`Erro ao editar ${itemType}: `, error.response.status, error.response.data);
               } else if (error.request) {
                 // A requisição foi feita mas não houve resposta
-                console.log(`Erro ao deletar ${itemType}:  Sem resposta do servidor`);
+                console.log(`Erro ao editar ${itemType}:  Sem resposta do servidor`);
               } else {
                 // Outro tipo de erro ocorreu
                 console.log("Erro desconhecido: ", error.message);
@@ -33,6 +33,6 @@ export default function useDeleteData() {
               
     }
     
-    return deleteData;
+    return saveData;
 
 }
